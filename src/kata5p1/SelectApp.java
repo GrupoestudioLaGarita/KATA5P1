431408849f5dc3b8efb3945d7b8ed352df273d59
Carlos <Carlos@DESKTOP-TUCUS6J>
2018-11-30 21:44:53 +0000

    KATA5P1

---
+++ src/kata5p1/SelectApp.java
@@ -0,0 +1,43 @@
+package kata5p1;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class SelectApp {
+
+    public Connection connect(){
+        String url = "jdbc:sqlite:KATA5.db";
+        Connection conn = null;
+        
+        try {
+            conn = DriverManager.getConnection(url);
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        
+        return conn;
+    }
+    
+    public void selectAll(){
+        String sql = "SELECT * FROM PEOPLE";
+        
+        try (Connection conn = this.connect();
+             Statement stmt = conn.createStatement();
+             ResultSet  rs = stmt.executeQuery(sql)){
+            
+            while (rs.next()){
+                System.out.println(rs.getInt("id") + "\t" + 
+                                   rs.getString("Nombre") + "\t" +
+                                   rs.getString("Apellidos") + "\t" +
+                                   rs.getString("Departamento") + "\t");
+            }
+            
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    
+}
